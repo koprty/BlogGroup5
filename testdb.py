bfc6bc4cf0e833d4ab13668156e0a5df45f84948
koprty <your_email@example.com>
2014-10-18 14:32:52 -0400

    working version

--- testdb.py
+++ testdb.py
@@ -11,14 +11,24 @@ c = conn.cursor()
 try:
     c.execute("CREATE TABLE post(name TEXT UNIQUE, content TEXT UNIQUE, date TEXT, author TEXT)")
     print "Creating new table for db"
-
 except:
-    print "Adding to created table"
+    print "Adding to current table"
 
 BASE = "INSERT INTO post VALUES('%(name)s', '%(content)s', '%(date)s', '%(author)s')"
-for l in csv.DictReader(open(csvname)):
-    q = BASE%l
-    print q
-    c.execute(q)
-
+try:
+    for l in csv.DictReader(open(csvname)):
+        q = BASE%l
+        print q
+        c.execute(q)
+except:
+    pass
 conn.commit()
+
+
+'''
+1. run sqlite3 blog.db
+example sqlite commands:
+select * from post;
+select content from post where name == "Welcome";
+
+
