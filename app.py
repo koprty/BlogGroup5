05da804d3b0a5b3ea0cd9cbd64f1e5e239210b28
vazzara <vazzara1@gmail.com>
2014-10-21 20:19:59 -0400

    added post stuff

--- app.py
+++ app.py
@@ -3,32 +3,35 @@ import sqlite3
 import csv
 app = Flask(__name__)
 
+conn = sqlite3.connect('blog.db')
+csvname = "data.csv"
+comcsvname = "comments.csv"
+c = conn.cursor()
+
 @app.route("/",methods=["GET","POST"])
 @app.route("/index", methods=["GET","POST"])
 def index():
-    initialize()
     if request.method == "GET":
         return render_template("index.html",posts=getPosts())
     else:
         pass
 
-@app.route("/post/<title>")
-def post(title=None,post=None):
-    return render_template("post.html",title=title,post=post,comments = getComments())
-
+@app.route("/post/<id>")
+def post(id=None):
+    thispost={}
+    for x in getPosts():
+        if x['id'] == id:
+            thispost=x
+    return render_template("post.html",post=thispost,comments=getComments())
 
 def initialize():
-    conn = sqlite3.connect('blog.db')
-    csvname = "data.csv"
-    comcsvname = "comments.csv"
-    c = conn.cursor()
     try:
-        c.execute("CREATE TABLE post(title TEXT UNIQUE, content TEXT, date TEXT, author TEXT)")
-        c.execute("CREATE TABLE comments(content TEXT, date TEXT, author TEXT)")
+        c.execute("CREATE TABLE post(id INTEGER UNIQUE, title TEXT UNIQUE, content TEXT, date TEXT, author TEXT)")
+        c.execute("CREATE TABLE comments(id INTEGER UNIQUE, content TEXT, date TEXT, author TEXT)")
         print "Creating new tables called 'post' and 'comments' in blog.db"
     except:
         print "Adding to table 'post' in blog.db"
-    BASE = "INSERT INTO post VALUES('%(title)s', '%(content)s', '%(date)s', '%(author)s')"
+    BASE = "INSERT INTO post VALUES('%(id)s','%(title)s', '%(content)s', '%(date)s', '%(author)s')"
     for l in csv.DictReader(open(csvname)):
         try:
             q = BASE%l
@@ -36,7 +39,7 @@ def initialize():
         except:
             pass
     conn.commit()
-    BASE = "INSERT INTO comments VALUES('%(content)s', '%(date)s', '%(author)s')"
+    BASE = "INSERT INTO comments VALUES('%(id)s','%(content)s', '%(date)s', '%(author)s')"
     for l in csv.DictReader(open(comcsvname)):
         try:
             q = BASE%l
@@ -45,13 +48,12 @@ def initialize():
             pass
     conn.commit()
 
+initialize()
 
 def getPosts():
-    csvname = "data.csv"
     return csv.DictReader(open(csvname))
 
 def getComments():
-    csvname = "comments.csv"
     return csv.DictReader(open(csvname))
 
 if __name__=="__main__":
